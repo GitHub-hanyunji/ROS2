ae888b16f89a0f1902f316c8736a3267112253ac
HanYunJi <151121556+GitHub-hanyunji@users.noreply.github.com>
2025-11-05 11:32:35 +0900

    Update main.cpp

--- ch19-RCLCPP/Project3/main.cpp
+++ ch19-RCLCPP/Project3/main.cpp
@@ -3,6 +3,20 @@
 #include <iostream>                 // 표준 입출력 라이브러리
 #include <memory>                   // 스마트 포인터를 사용하기 위한 헤더파일
 #include <chrono>                   // 시간 관련 헤더파일
+#include <functional>               // 함수 헤더파일
+
+using namespace std::chrono_literals;  // 시간 리터럴
+
+void callback(rclcpp::Node::SharedPtr node, rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr mypub,
+    geometry_msgs::msg::Twist message)
+{   
+    // 거북이 회전
+    message.linear.x=2.0; message.linear.y=0.0; message.angular.z=2.0;
+    // 로그 출력
+    RCLCPP_INFO(node->get_logger(), "Publish linear: %f %f %f, angular: %f %f %f", message.linear.x,message.linear.y,message.linear.z,
+                message.angular.x,message.angular.y,message.angular.z);
+    mypub->publish(message);  // 토픽 발행
+}
 
 // main 함수
 int main(int argc, char * argv[])
@@ -12,50 +26,11 @@ int main(int argc, char * argv[])
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));       // qos 객체 생성
     //publisher 생성 turtle1/cmd_vel 로 보냄
     auto mypub = node->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel",qos_profile );
-    geometry_msgs::msg::Twist message;
-
-    char c;
-    rclcpp::WallRate loop_rate(1.0); //반복주파수를 저장하는 객체(단위 Hz)  1초에 보내고 싶은 메시지 수
-    while(rclcpp::ok())
-    {
-        std::cin>>c;   // 사용자로부터 문자 입력 받음
-        // 'f' 입력시 거북이 속도 2로 앞으로 전진
-        if(c=='f'){
-            message.linear.x=2.0;
-            message.linear.y=0.0;
-            message.angular.z=0.0;
-        }
-        // 'b' 입력시 거북이 속도 2로 뒤로 후진
-        else if(c=='b'){
-            message.linear.x=-2.0;
-            message.linear.y=0.0;
-            message.angular.z=0.0;
-        }
-        // 'l' 입력시 거북이 속도 2로 좌회전
-        else if(c=='l'){
-            message.linear.x=0.0;
-            message.linear.y=0.0;
-            message.angular.z=2.0;
-        }
-        // 'r' 입력시 거북이 속도 2로 우회전
-        else if(c=='r'){
-            message.linear.x=0.0;
-            message.linear.y=0.0;
-            message.angular.z=-2.0;
-        }
-        // 그외의 입력에서는 거북이 안 움직임
-        else{
-            message.linear.x=0.0;
-            message.linear.y=0.0;
-            message.angular.z=0.0;
-        }
-        // 로그 출력
-        RCLCPP_INFO(node->get_logger(), "Publish linear: %f %f %f, angular: %f %f %f", message.linear.x,message.linear.y,message.linear.z,
-                    message.angular.x,message.angular.y,message.angular.z);
-        mypub->publish(message);  // 토픽 발행
-        //rclcpp::spin_some(node);
-        loop_rate.sleep(); //반복주파수에서 남은 시간 만큼 sleep
-    }
+    geometry_msgs::msg::Twist message;  // Twist 메시지 객체 생성
+    // bind 함수로 callback 함수를 매개변수 없는 void()함수형태로 만들기
+    std::function<void()>fn=std::bind(callback,node,mypub,std::ref(message));
+    auto timer=node->create_wall_timer(1s,fn);  // 1초마다 callback 함수 호출
+    rclcpp::spin(node);  // 프로그램 계속 실행
     rclcpp::shutdown();  // ros2 종료
     return 0; 
 }
