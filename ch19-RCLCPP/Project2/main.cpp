fbf7d988c1d6293300843c4d42d2a1894b31277d
HanYunJi <151121556+GitHub-hanyunji@users.noreply.github.com>
2025-11-05 11:23:23 +0900

    Update main.cpp

--- ch19-RCLCPP/Project2/main.cpp
+++ ch19-RCLCPP/Project2/main.cpp
@@ -3,7 +3,18 @@
 #include <iostream>                 // 표준 입출력 라이브러리
 #include <memory>                   // 스마트 포인터를 사용하기 위한 헤더파일
 #include <chrono>                   // 시간 관련 헤더파일
+#include <functional>
 
+using namespace std::chrono_literals;  // 시간 리터럴
+
+// callback 함수
+void callback(rclcpp::Node::SharedPtr node, rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr mypub,
+    geometry_msgs::msg::Vector3 message)
+{
+    // 로그 출력
+    RCLCPP_INFO(node->get_logger(), "Publish: %f %f %f", message.x,message.y,message.z);
+    mypub->publish(message);  // 토픽 발행
+}
 // main 함수
 int main(int argc, char * argv[])
 {
@@ -15,16 +26,10 @@ int main(int argc, char * argv[])
     geometry_msgs::msg::Vector3 message;    // Vector3 메시지 객체 생성
     // Vector3의 x,y,z 사용자로부터 입력 받기
     std::cin>>message.x>>message.y>>message.z;
-    rclcpp::WallRate loop_rate(1.0); //반복주파수를 저장하는 객체(단위 Hz)  1초에 보내고 싶은 메시지 수
-    // 무한루프 -> Ctrl+c 입력 받으면 종료
-    while(rclcpp::ok())
-    {
-        // 로그 출력
-        RCLCPP_INFO(node->get_logger(), "Publish: %f %f %f", message.x,message.y,message.z);
-        mypub->publish(message);  // 토픽 발행
-        //rclcpp::spin_some(node);
-        loop_rate.sleep(); //반복주파수에서 남은 시간 만큼 sleep
-    }
+    // bind 함수로 callback 함수를 매개변수 없는 void()함수형태로 만들기
+    std::function<void()>fn=std::bind(callback,node,mypub,message);
+    auto timer=node->create_wall_timer(1s,fn);  // 1초마다 callback 함수 호출
+    rclcpp::spin(node);  // 프로그램 계속 실행
     rclcpp::shutdown();   // ros2 종료
-    return 0;
+    return 0;  
 }
