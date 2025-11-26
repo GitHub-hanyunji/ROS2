7b5bf5cacb36386040dd4a8dec0adcdaa66b831c
HanYunJi <151121556+GitHub-hanyunji@users.noreply.github.com>
2025-11-26 15:31:27 +0900

    Update sub.cpp

--- ch24_ROS2_dxl_2/project1/camera/jetson/src/sub.cpp
+++ ch24_ROS2_dxl_2/project1/camera/jetson/src/sub.cpp
@@ -4,39 +4,24 @@
 #include <memory>
 #include <functional>
 #include <iostream>
-
 using std::placeholders::_1;
-
+  
 void mysub_callback(rclcpp::Node::SharedPtr node, const sensor_msgs::msg::CompressedImage::SharedPtr msg)
 {
-    // 압축된 이미지를 디코딩하여 Mat으로 변환
     cv::Mat frame = cv::imdecode(cv::Mat(msg->data), cv::IMREAD_COLOR);
-    // 그레이 영상 변환
-    cv::Mat gray;
-    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-
-    // 이진 영상 생성 (threshold)
-    cv::Mat binary;
-    cv::threshold(gray, binary, 128, 255, cv::THRESH_BINARY);
-
-    // 각각 영상 출력
-    cv::imshow("Original", frame);
-    cv::imshow("Gray", gray);
-    cv::imshow("Binary", binary);
-
+    cv::imshow("wsl",frame);
     cv::waitKey(1);
     RCLCPP_INFO(node->get_logger(), "Received Image : %s,%d,%d", msg->format.c_str(),frame.rows,frame.cols);
 }
-
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("camsub_wsl_13");
+    auto node = std::make_shared<rclcpp::Node>("camsub_wsl");
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)); //TCP
     //auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(); //UDP
     std::function<void(const sensor_msgs::msg::CompressedImage::SharedPtr msg)> fn;
     fn = std::bind(mysub_callback, node, _1);
-    auto mysub = node->create_subscription<sensor_msgs::msg::CompressedImage>("image/compressed_13",qos_profile,fn);
+    auto mysub = node->create_subscription<sensor_msgs::msg::CompressedImage>("image/compressed",qos_profile,fn);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
